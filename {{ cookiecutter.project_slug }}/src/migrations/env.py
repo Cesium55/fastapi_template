551c3fa41fabe449c173baf4692779fd3e1afe6a
Cesium55 <mksmtshkn@gmail.com>
2026-04-06 23:05:26 +0300

    migrations env.py fix

--- {{ cookiecutter.project_slug }}/src/migrations/env.py
+++ {{ cookiecutter.project_slug }}/src/migrations/env.py
@@ -5,8 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from models import Base
-from config import settings
+from basic_utils.models import Base
+from basic_utils.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
